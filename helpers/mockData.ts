2180bae3188b9d022a7737ea60ea74d9f111f8d7
duclm <ducle7144@gmail.com>
2022-09-30 10:20:12 +0700

    Remove code

--- helpers/mockData.ts
+++ helpers/mockData.ts
@@ -1,43 +1,4 @@
 import { IconUserPlus, IconDiscount2, IconReceipt2, IconCoin } from "@tabler/icons";
-import { RowData } from "../models";
-
-export const sampleProductList: RowData[] = [
-    {
-        id: "1",
-        name: "Bánh tráng muối ớt",
-        stock: "50",
-        sold: "10",
-        price: "25000",
-    },
-    {
-        id: "2",
-        name: "Bánh tráng trộn",
-        stock: "50",
-        sold: "15",
-        price: "35000",
-    },
-    {
-        id: "3",
-        name: "Bánh tráng bơ tỏi",
-        stock: "100",
-        sold: "20",
-        price: "45000",
-    },
-    {
-        id: "4",
-        name: "Bánh tráng cháy đen",
-        stock: "100",
-        sold: "25",
-        price: "55000",
-    },
-    {
-        id: "5",
-        name: "Bánh tráng",
-        stock: "200",
-        sold: "30",
-        price: "65000",
-    },
-];
 
 export const stats = {
     data: [
--- pages/product-list.tsx
+++ pages/product-list.tsx
@@ -1,7 +1,5 @@
 import { NextPageContext } from "next";
 import TableData from "../components/TableData/TableData";
-import { getAllProduct } from "../helpers/apiCall";
-import { sampleProductList } from "../helpers/mockData";
 
 export default function App() {
     return (
